71111071c525b2c792b6ae9edd4f3b310da1807e
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-05 12:15:06 +0530

    Add files via upload

---
+++ integerarray.java
@@ -0,0 +1,21 @@
+package kas;
+import java.util.*;
+public class integerarray {
+	public static void main(String args[]){
+		int array[]=new int[10];
+		Scanner s=new Scanner(System.in);
+		for(int i=0; i<array.length; i++){
+			array[i]=s.nextInt();
+			
+		}
+		for( int i : array){
+			if(array[i]==i){
+				System.out.println("the number" +array[i]+"equal to its index"+i);
+				
+			}
+		}
+		s.close();
+	
+	}
+
+}
